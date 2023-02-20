2c0277b0ccf4539921f5e5063afb9c612d36b2ec
OlegaDance <oleg.mihalishy@gmail.com>
2023-02-20 20:15:25 +0200

    Auth&register

--- BASA2/MainWindow.xaml.cs
+++ BASA2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,53 @@ namespace BASA2
         {
             InitializeComponent();
         }
+        public bool IsDarkTheme { get; set; }
+        public static System.Windows.Media.Color ControlLightColor { get; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+
+
+        private void ThemToogle_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            ITheme theme = paletteHelper.GetTheme();
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+                LabelName.Foreground = Brushes.Black;
+                LabelName1.Foreground = Brushes.Black;
+                LabelName2.Foreground = Brushes.Black;
+                LabelName3.Foreground = Brushes.Black;
+                LabelName4.Foreground = Brushes.Black;
+
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+                LabelName.Foreground = Brushes.White;
+                LabelName1.Foreground = Brushes.White;
+                LabelName2.Foreground = Brushes.White;
+                LabelName3.Foreground = Brushes.White;
+                LabelName4.Foreground = Brushes.White;
+
+
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
     }
 }
