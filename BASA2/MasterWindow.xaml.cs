47bd01e81cec718b1f93b17bca37631972593e64
OlegaDance <oleg.mihalishy@gmail.com>
2023-02-20 20:20:24 +0200

    Search&MyprofileUpdate

--- BASA2/MasterWindow.xaml.cs
+++ BASA2/MasterWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,5 +24,33 @@ namespace BASA2
         {
             InitializeComponent();
         }
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+
+
+        private void ThemToogle_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            ITheme theme = paletteHelper.GetTheme();
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+
+
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+
+
+
+            }
+            paletteHelper.SetTheme(theme);
+        }
     }
 }
+    
+
