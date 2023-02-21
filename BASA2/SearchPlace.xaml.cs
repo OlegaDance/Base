abe3b44379e5bac990aff34c8b49ef7df031599a
OlegaDance <oleg.mihalishy@gmail.com>
2023-02-22 00:36:45 +0200

    LoadingUpdate

--- BASA2/SearchPlace.xaml.cs
+++ BASA2/SearchPlace.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,5 +29,6 @@ namespace BASA2
             Goods.DataContext= bd.Products.ToList();
             Goods.ItemsSource = bd.Products.ToList();
         }
+       
     }
 }
